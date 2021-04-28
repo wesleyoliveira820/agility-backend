ae6a64fcb421d3e574999309dd16745d806fedc9
wesleyoliveira820 <wesleyoliveiradeveloper@gmail.com>
2021-04-28 16:35:06 -0300

    style: added the eslint and prettier set

--- app/Models/Traits/NoTimestamp.js
+++ app/Models/Traits/NoTimestamp.js
@@ -1,16 +1,14 @@
-'use strict'
-
 class NoTimestamp {
-  register (Model) {
+  register(Model) {
     Object.defineProperties(Model, {
       createdAtColumn: {
         get: () => null,
       },
       updatedAtColumn: {
         get: () => null,
-      }
-    })
+      },
+    });
   }
 }
 
-module.exports = NoTimestamp
+module.exports = NoTimestamp;
--- server.js
+++ server.js
@@ -1,25 +1,7 @@
-'use strict'
+const { Ignitor } = require('@adonisjs/ignitor');
+const fold = require('@adonisjs/fold');
 
-/*
-|--------------------------------------------------------------------------
-| Http server
-|--------------------------------------------------------------------------
-|
-| This file bootstraps Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
-|
-| """ Loading ace commands """
-|     At times you may want to load ace commands when starting the HTTP server.
-|     Same can be done by chaining `loadCommands()` method after
-|
-| """ Preloading files """
-|     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass a relative path from the project root.
-*/
-
-const { Ignitor } = require('@adonisjs/ignitor')
-
-new Ignitor(require('@adonisjs/fold'))
+new Ignitor(fold)
   .appRoot(__dirname)
   .fireHttpServer()
-  .catch(console.error)
+  .catch(console.error);
