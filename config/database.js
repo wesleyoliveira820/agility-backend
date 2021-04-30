bd5f7a5fa3fe0a90826ff338d2c2bd50eaa1e9b8
wesleyoliveira820 <wesleyoliveiradeveloper@gmail.com>
2021-04-30 20:18:45 -0300

    refactor: changes to the test environment and database configuration variables

--- config/database.js
+++ config/database.js
@@ -8,10 +8,10 @@ module.exports = {
     client: 'pg',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
+      port: Env.get('DB_PORT', '', 5432),
+      user: Env.get('DB_USER', 'postgres'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
+      database: Env.get('DB_DATABASE', 'agility'),
     },
     debug: Env.get('DB_DEBUG', false),
   },
